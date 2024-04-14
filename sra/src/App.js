02f2b2733d570f0e5aa9a12a7ff8bf79c8805eb4
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2024-04-14 18:19:36 -0500

    flameo hotman?

---
+++ sra/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import AboutUs from './components/AboutUs.js';
+import Home from './components/Home.js';
+import Research from './components/Research.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Research />
+      <AboutUs />
+    </div>
+  );
+}
+
+
+export default App;
