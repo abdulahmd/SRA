88ddd474704a1750b221363239d955fd9e51f1cd
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2024-04-14 18:36:07 -0500

    Pls Gaangy

---
+++ sra/src/components/AboutUs.js
@@ -0,0 +1,15 @@
+import './AboutUs.css';
+
+
+function AboutUs() {
+  return (
+    <div className="AboutUs">
+        <div className="abt">
+            <h3>About Us</h3>
+        </div>
+    </div>
+  );
+}
+
+
+export default AboutUs;
